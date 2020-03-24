841209ca88cbdaf500ca45d20626dd2e93781b43
cleytinho <cleytonlm@outlook.com>
2020-03-24 14:57:13 -0300

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Footer from './Components/Footer/footer';
+import Footer from './Components/Pagina_Produto/paginaproduto';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+   
     <Footer/>
   );
 }
